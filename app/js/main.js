4fefcb6447823e4dff5fd45b5173e680664f48b3
10hokageee <nikita.serdyukov.20033002@gmail.com>
2023-05-31 01:01:48 +0300

    fixed icons, added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
 $(function(){
-
+ $('.reviews__inner').slick({
+  dots: true,
+  prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/prev-arrow.svg" alt=""</button>',
+  nextArrow: '<button type="button" class="slick-next"><img src="images/icons/next-arrow.svg" alt=""</button>'
+ });
 });
 
 
