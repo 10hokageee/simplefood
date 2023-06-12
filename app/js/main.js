45d03eae9e38be7ff9dc766d3ece31db056474c2
10hokageee <nikita.serdyukov.20033002@gmail.com>
2023-06-12 22:16:20 +0300

    fixed

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,12 @@
 $(function(){
  $('.reviews__inner').slick({
   dots: true,
-  prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/prev-arrow.svg" alt=""</button>',
-  nextArrow: '<button type="button" class="slick-next"><img src="images/icons/next-arrow.svg" alt=""</button>'
+  prevArrow: '<button type="button" class="slick-prev"><svg><use xlink:href="images/sprite.svg#prev-arrow"></use></svg></button>',
+  nextArrow: '<button type="button" class="slick-next"><svg><use xlink:href="images/sprite.svg#next-arrow"></use></svg></button>'
  });
 });
 
 
 var mixitup = mixitup('.features-list__items')
+
 
